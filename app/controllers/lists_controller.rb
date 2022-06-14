2209348d3f409b5a2f51a885929c2d4e3e613155
Jasperzhang <zed0416660907@gmail.com>
2022-06-14 20:48:21 +1000

    watch-list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
@@ -16,22 +16,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-    redirect_to list_path(@list)
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
-    redirect_to root_path
+    redirect_to lists_path
   end
 
   private
 
-  def list_params
-    params.require(:list).permit(:name, :imageUrl)
+  def set_list
+    @list = List.find(params[:id])
   end
 
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
